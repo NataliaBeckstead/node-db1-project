d8734ecac27c091344c17c76869127c4de555bfa
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-13 14:08:22 -0600

    set up project, get request to api/accounts

--- api/server.js
+++ api/server.js
@@ -6,4 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json({ data: accounts})
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: error.message});
+        })
+});
+
 module.exports = server;
